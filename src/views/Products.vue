41f3f58d72267fe5b08f6fe764b3bdf12011ce3b
Philip Nwabuwa <philipnwabuwa@gmail.com>
2023-03-17 16:51:22 +0100

    Added pagination

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -6,10 +6,33 @@
     <div v-else>
       <div v-if="products.length">
         <ul>
-          <li v-for="product in products" :key="product.id">
-            {{ product.title }}
+          <li v-for="product in currentPageProducts" :key="product.id">
+            {{ product.title }} {{ product.price }}
           </li>
         </ul>
+        <div>
+          <span>Page {{ currentPage }} of {{ totalPages }}</span>
+
+          <div>
+            <button :disabled="currentPage === 1" @click="currentPage--">
+              Prev
+            </button>
+            <button
+              v-for="page in pages"
+              :key="page"
+              @click="setCurrentPage(page)"
+              :class="{ active: currentPage === page }"
+            >
+              {{ page }}
+            </button>
+            <button
+              :disabled="currentPage * perPage >= products.length"
+              @click="currentPage++"
+            >
+              Next
+            </button>
+          </div>
+        </div>
       </div>
       <div v-else>
         <p>No products available.</p>
@@ -27,16 +50,29 @@ export default {
 
   computed: {
     ...mapGetters(["isAuthenticated"]),
-    // user() {
-    //   const { user } = useAuth();
-    //   return user ? user.name : "";
-    // },
+    currentPageProducts() {
+      const start = (this.currentPage - 1) * this.perPage;
+      const end = start + this.perPage;
+      return this.products.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.products.length / this.perPage);
+    },
+    pages() {
+      const pages = [];
+      for (let i = 1; i <= this.totalPages; i++) {
+        pages.push(i);
+      }
+      return pages;
+    },
   },
   data() {
     return {
       products: [],
       loading: false,
       error: null,
+      perPage: 10,
+      currentPage: 1,
     };
   },
   created() {
@@ -45,7 +81,6 @@ export default {
       .get("https://dummyjson.com/products")
       .then((response) => {
         this.products = response.data.products;
-        console.log(this.products);
       })
       .catch((error) => {
         this.error = error.message;
@@ -53,6 +88,11 @@ export default {
       .finally(() => {
         this.loading = false;
       });
+  },
+  methods: {
+    setCurrentPage(page) {
+      this.currentPage = page;
+    },
   },
   mounted() {
     if (!this.isAuthenticated) {
